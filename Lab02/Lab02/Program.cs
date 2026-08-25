7a2604f73a6ec6607e9c037537120982ec693066
cheesepie001 <poramat.chua@bumail.net>
2026-08-25 11:18:40 +0700

    Add lab02

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -1,5 +1,12 @@
-﻿namespace Lab2
-{
+﻿namespace Lab02
+{/*
+ * Student ID : 1690702772
+ * Name       : นายปรมัตถื เชื้อเมืองพาน
+ * Section    : 129C
+ * No.        : N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+
     internal class Program
     {
         static void Main(string[] args)
