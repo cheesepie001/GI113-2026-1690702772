d8de09712adc28acecc2514a7e62b8af945703b4
cheesepie001 <poramat.chua@bumail.net>
2026-09-15 10:03:17 +0700

    Add Lab05

---
+++ Lab05/Program.cs
@@ -0,0 +1,15 @@
+﻿// ชื่อ - นามสกุล: นายปรมัตถ์ เชื้อเมืองพาน
+// Section: 129C
+// รหัสนักศึกษา: 1690702772
+// เลขที่: 23
+
+namespace Lab05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
